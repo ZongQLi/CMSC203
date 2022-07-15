d3c80a7b3c23027e1772115ad4f948c70e51ee3b
ZongQLi <107369067+ZongQLi@users.noreply.github.com>
2022-07-15 15:48:01 -0400

    Update ManagementCompany.java

--- Assignment 4/ManagementCompany.java
+++ Assignment 4/ManagementCompany.java
@@ -45,19 +45,40 @@ public class ManagementCompany()
   // Copy Constructor
   public ManagementCompany(ManagementCompany otherCompany)
   {
-    this.properties = otherCompany.properties;
+    this.name = otherCompany.name;
+    this.taxID = otherCompany.taxID;
+    this.mgmFeePer = otherCompany.mgmFeePer;
+    properties = new Property[MAX_PROPERTY];
   }
   
-  // Method addProperty Version 1
-  public void addProperty(Property object)
+  // The getMAX_PROPERTY method returns the MAX_PROPERTY constant that represents the size of
+  // the properties array
+  public int getMAX_PROPERTY()
   {
-    properties = object.ManagementCompany();
+    return MAX_PROPERTY;
+  }
+  
+  // The first version of the addProperty method performs the following:
+  // Adds the property object to the "properties" array
+  // Returns either -1 if the array is full, -2 if the property is null,
+  //                -3 if the plot is not contained by the MgmtCo plot,
+  //                -4 of the plot overlaps any other property, or 
+  //                the index in the array where the property was added successfully
+  // Method addProperty Version 1
+  public int addProperty(Property property)
+  {
+    int value = null;
+    
+    if(property.length == MAX_PROPERTY)
+      value = -1;
+    if(property.lenth == null)
+      value = -2;
   }
   
   // Method addProperty Version 2
   public void addProperty(String propertyName, String city, double rent, String ownerName)
   {
-      
+    
   }
   
   // Method addProperty Version 3
