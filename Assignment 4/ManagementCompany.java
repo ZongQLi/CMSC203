2095b39b70fa72f4d7d3b24d853d37c115de73dc
ZongQLi <107369067+ZongQLi@users.noreply.github.com>
2022-07-15 14:33:49 -0400

    Update ManagementCompany.java

--- Assignment 4/ManagementCompany.java
+++ Assignment 4/ManagementCompany.java
@@ -21,24 +21,43 @@ public class ManagementCompany()
     mgmFeePer = 0.0;
   }
   
-  // Copy Constructor
+  // Constructor 2
   public ManagementCompany(String name, String taxID, double mgmFee)
   {
     this.name = name;
     this.taxID = taxID;
     this.mgmFeePer = mgmFee;
+    plot = new Plot();
+    properties = new Property[MAX_PROPERTY];
+  }
+  
+  // Constructor 3
+  public ManagementCompany(String name, String taxID, double mgmFee,
+                           int x, int y, int width, int depth)
+  {
+    this.name = name;
+    this.taxID = taxID;
+    this.mgmFeePer = mgmFee;
+    plot = new Plot();
+    properties = new Property[MAX_PROPERTY];
+  }
+  
+  // Copy Constructor
+  public ManagementCompany(ManagementCompany otherCompany)
+  {
+    this.properties = otherCompany.properties;
   }
   
   // Method addProperty Version 1
   public void addProperty(Property object)
   {
-      properties = object.ManagementCompany();
+    properties = object.ManagementCompany();
   }
   
   // Method addProperty Version 2
   public void addProperty(String propertyName, String city, double rent, String ownerName)
   {
-    
+      
   }
   
   // Method addProperty Version 3
