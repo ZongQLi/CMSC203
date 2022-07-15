8ca45fa3f090160f67bd2cdaf5b56b950702f8a6
ZongQLi <107369067+ZongQLi@users.noreply.github.com>
2022-07-15 11:51:27 -0400

    Create ManagementCompany.java

---
+++ Assignment 4/ManagementCompany.java
@@ -0,0 +1,41 @@
+import java.util.String;
+
+// Class
+public class ManagementCompany()
+{
+  private String name;
+  private int taxID;
+  private final double MAX_PROPERTY = 5;
+  private Property properties;
+  private final double MGMT_WIDTH = 10;
+  private final double MGMT_HEIGHT = 10;
+  
+  // Constructor
+  public ManagementCompany()
+  { 
+  }
+  
+  // Copy Constructor
+  public ManagementCompany()
+  {
+  }
+  
+  // Method addProperty Version 1
+  public void addProperty(Property object)
+  {
+      properties = object.ManagementCompany();
+  }
+  
+  // Method addProperty Version 2
+  public void addProperty(String propertyName, String city, double rent, String ownerName)
+  {
+    
+  }
+  
+  // Method addProperty Version 3
+  public void addProperty(String propertyName, String city, double rent, String ownerName,
+                          int x, int y, int width, int depth)
+  {
+    
+  }
+}
